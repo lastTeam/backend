19fc56c1f4830f5265855aeda5756d064b2511ae
khalilo93 <khalil.mahjoub@hotmail.fr>
2024-10-31 16:35:31 +0100

    cart

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const app = express();
 
 
 
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,7 +29,8 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/category", categoryRoutes);
-app.use("/api" ,getProdBytitleRouter )
+app.use("/api", router);
+app.use("/api/getProd" ,getProdBytitleRouter )
 // Error handler middleware
 app.use((err, req, res, next) => {
   res
@@ -35,13 +38,7 @@ app.use((err, req, res, next) => {
     .json({ message: err.message || "Internal Server Error" });
 });
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/chats", chatRoutes);
-app.use("/api/reviews", reviewRoutes);
-app.use("/api", router);
-app.use("/api/category", categoryRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
